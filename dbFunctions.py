b8d10169219447de1a1d45476971d9cc31657a23
Mark <mgseverson83@gmail.com>
2023-02-28 05:39:48 -0500

    Cleaned up code Added client and product tuples Ensured creation of DB and tables with columns and primary keys Began Product Table Functions Still need modify price per unit, sale price and total amount

--- dbFunctions.py
+++ dbFunctions.py
@@ -1,16 +1,16 @@
-stringNotNull = f'NOT NULL'
-stringUnique = f'UNIQUE'
-stringAutoIncrement = f'AUTO_INCREMENT'
-stringCreateDBTable = f'CREATE TABLE'
-stringCreateDB = f'CREATE DATABASE'
-stringAlterTable = f'ALTER TABLE'
-stringModColumn = f'MODIFY COLUMN'
-stringInsertInto = f'INSERT INTO'
-stringValues = f'VALUES'
-stringDropTable = f'DROP TABLE'
-stringDeleteFrom = f'DELETE FROM'
-stringWhere = f'WHERE'
-stringPrimaryKey = f'PRIMARY KEY'
+# stringNotNull = f'NOT NULL'
+# stringUnique = f'UNIQUE'
+# stringAutoIncrement = f'AUTO_INCREMENT'
+# stringCreateDBTable = f'CREATE TABLE'
+# stringCreateDB = f'CREATE DATABASE'
+# stringAlterTable = f'ALTER TABLE'
+# stringModColumn = f'MODIFY COLUMN'
+# stringInsertInto = f'INSERT INTO'
+# stringValues = f'VALUES'
+# stringDropTable = f'DROP TABLE'
+# stringDeleteFrom = f'DELETE FROM'
+# stringWhere = f'WHERE'
+# stringPrimaryKey = f'PRIMARY KEY'
 
 #TESTED WORKING
 def columnNameTypeString(columnDict:dict):   
@@ -27,47 +27,30 @@ def columnNameTypeString(columnDict:dict):
                 value = columnDict.get(i)
                 columnNameTypeString += name + " " + value
         counter = 0
-        print(columnNameTypeString)
         return columnNameTypeString
 
-#TESTED WORKING
 def createColumnDict(columnNameList:list, columnTypeList:list):
     columnDict = {}
     for i in columnNameList:
         columnDict.update({columnNameList[len(columnDict)]: columnTypeList[len(columnDict)]})
     return columnDict
 
-#TESTED WORKING ON 02/26/2023 @ 20:30
-def createDBTable(connection:object, cursor:object, tableName:str, columnNameTypeString:str): 
-    command = stringCreateDBTable + ' ' + tableName + ' '
-    splitNameType = columnNameTypeString.split(', ')
-    primaryKeyAndType = splitNameType[0]
-    splitNameType.pop(0)
-    command += '(' + primaryKeyAndType + ' ' + stringNotNull + ' ' + stringUnique + ' ' + stringAutoIncrement + ', '
-
-    check = len(splitNameType)
-    for i in splitNameType:
-        command += i + ', '
-
-    primaryKey = primaryKeyAndType.split(' ')
-    primaryKey.pop(1)
-    primaryKey = str(primaryKey[0])
-    command += stringPrimaryKey + ' (' + primaryKey + '));'
-
+def createDatabase(connection:object, cursor:object, dbName:str):
+    command = f'CREATE DATABASE {dbName}'
     cursor.execute(command)
     connection.commit()
 
-#WILL NO LONGER BE USED
-# def createPrimary(connection:object, cursor:object, tableName:str, primaryColumn:str, columnDict:dict): 
-#     command = f'ALTER TABLE {tableName} MODIFY COLUMN {primaryColumn} {columnDict.get(primaryColumn)} PRIMARY KEY ({primaryColumn});'
-#     cursor.execute(command)
-#     connection.commit()
+def createTable(connection:object, cursor:object, columnNameType:str, tableName:str):
+    command = f'CREATE TABLE {tableName} ({columnNameType});'
+    cursor.execute(command)
+    connection.commit()
 
-def addFront(connection:object, cursor:object, tableName:str, values:list): 
-    command = f'INSERT INTO clients (ClientFirst, ClientBalance) VALUES (%s,%s)'
+
+#Client Table functions
+def addFront(connection:object, cursor:object, values:list): 
+    command = f'INSERT INTO clients (ClientFirst, ClientLast, ClientBalance) VALUES (%s,%s,%s)'
     cursor.execute(command, values)
     connection.commit()
-    # cursor.execute('SELECT * FROM clients')
 
 def addToBalance(connection:object, cursor:object, tableName:str, clientID:str, balanceAdd:int): 
     command = f'SELECT ClientBalance FROM {tableName} WHERE ClientID={clientID}'
@@ -79,11 +62,6 @@ def addToBalance(connection:object, cursor:object, tableName:str, clientID:str, 
     cursor.execute(command)
     connection.commit()
 
-def johnnyDropTables(connection:object, cursor:object, tableName):
-    command = f'DROP TABLE {tableName};'
-    cursor.execute(command)
-    connection.commit()
-
 def makePayment(connection:object, cursor:object, tableName:str, clientID:int, *clientFirst:str, repayAmount:int):
     command = f'SELECT ClientBalance FROM {tableName} WHERE ClientID={clientID}'
     cursor.execute(command)
@@ -91,5 +69,17 @@ def makePayment(connection:object, cursor:object, tableName:str, clientID:int, *
     test = test[0]
     final = test - repayAmount
     command = f'UPDATE {tableName} SET ClientBalance={final} WHERE ClientID={clientID};'
+    cursor.execute(command)
+    connection.commit()
+
+
+#Product Table Functions
+def addProducts(connection:object, cursor:object, values:list):
+    command = f'INSERT INTO products (ProductName, ProductCostPerUnit, ProductSalePerUnit, ProductTotalAmount) VALUES (%s,%s,%s,%s)'
+    cursor.execute(command, values)
+    connection.commit()
+
+def johnnyDropTables(connection:object, cursor:object, tableName):
+    command = f'DROP TABLE {tableName};'
     cursor.execute(command)
     connection.commit()
--- moneyBaggYoooDB.py
+++ moneyBaggYoooDB.py
@@ -7,38 +7,63 @@ dbHost = 'localhost'
 dbPort = 3306
 dbUserRoot = 'root'
 dbRootPassword = 'toor'
-dbName = 'moneyBaggYoooDB'
+dbName = 'moneybaggyooodb'
 clientTable = 'clients'
 productTable = 'products'
 productColumnNames = ['ProductID', 'ProductName', 'ProductPPU', 'ProductSellPrice']
 productColumnTypes = ['int', 'varchar(255)', 'float', 'float']
 clientColumnNames = ['ClientID', 'ClientFirst', 'ClientBalance']
 clientColumnTypes = ['int', 'varchar(255)', 'float']
+clientColumnNameTypeString = 'ClientID int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, ClientFirst varchar(255), ClientLast varchar(255), ClientBalance float'
+productColumnNameTypeString = 'ProductID int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, ProductName varchar(255), ProductCostPerUnit float, ProductSalePerUnit float, ProductTotalAmount float'
 tables = [clientTable, productTable]
 
 #Connect to DB if Exists
 try:
-    connection = msql.connect(host=dbHost, port=dbPort, user=dbUserRoot, password=dbRootPassword, database=dbName)
+    connection = msql.connect(host=dbHost, port=dbPort, user=dbUserRoot, password=dbRootPassword)#, database=dbName)
+    cursor = connection.cursor()
 except(msql.DatabaseError):
     print("Error, not connected")
 
-#Create DB cursor object for interacting with db/running queries
-cursor = connection.cursor()
+try:
+    dbf.createDatabase(connection, cursor, dbName)
+except (msql.DatabaseError):
+    print('DB already exists')
+
+try:
+    connection = msql.connect(host=dbHost, port=dbPort, user=dbUserRoot, password=dbRootPassword, database=dbName)
+    cursor = connection.cursor()
+except(msql.DatabaseError):
+    print('Unable to connect to ' + dbName)
+
+try:
+    dbf.createTable(connection, cursor, clientColumnNameTypeString, clientTable)
+except:
+    msql.ProgrammingError(errno=1050)
+    print("Clients table already exists")
+
+try:
+    dbf.createTable(connection, cursor, productColumnNameTypeString, productTable)
+except:
+    msql.ProgrammingError(errno=1050)
+    print("Products table already exists")
+
+clientTupleList = [('John', 'Paz', '100'), ('Chico', 'Marino', '50'), ('Mike', 'Hummer', '200'), ('Walt', 'MindyMan', '50'), ('Stone', 'Lingo', '70'), ('Striker', 'VonFireworkin', '60'), ('Lee', 'Redard', '40'), ('Alexa', 'TashiFriend', '20')]
+productTupleList = [('Go Green Certificate', '4.28', '10.00', '28'), ('Software License', '32.14', '100.00', '20'), ('Software Hardening License', '32.14', '120.00', '0'), ('Modified Software License', '21.53', '80.00', '0')]
+
+for i in clientTupleList:
+    dbf.addFront(connection, cursor, i)
+
+for i in productTupleList:
+    dbf.addProducts(connection, cursor, i)
 
 #Create 'clients' column dictionary and the string of both names and types and DB table
 clientColumnDict = dbf.createColumnDict(clientColumnNames, clientColumnTypes)
 clientColumnNameTypeString = dbf.columnNameTypeString(clientColumnDict)
-# dbf.createDBTable(connection, cursor, clientTable, clientColumnNameTypeString)
 
 # #Create 'products' column dictionary and the string of both names and types and DB table
 productColumnDict = dbf.createColumnDict(productColumnNames, productColumnTypes)
 productColumnNameTypeString = dbf.columnNameTypeString(productColumnDict)
-# dbf.createDBTable(connection, cursor, productTable, productColumnNameTypeString)
-
-dbValues = (8, 80)
-# dbf.makePayment(connection, cursor, clientTable, 8, repayAmount=15)
-# dbf.addFront(connection, cursor, clientTable, dbValues)
-dbf.addToBalance(connection, cursor, clientTable, dbValues[0], dbValues[1])
 
 #Close DB cursor
 cursor.close()
