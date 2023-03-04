faf00c989a65ac387aec1294e3255c7805930b5d
Krongi <mgseverson83@gmail.com>
2023-03-04 13:50:32 -0500

    removed commented code

--- moneyBaggYoooDB.py
+++ moneyBaggYoooDB.py
@@ -2,22 +2,6 @@
 import mysql.connector as msql
 import dbFunctions as dbf
 
-#Init variablees
-# dbHost = 'localhost'
-# dbPort = 3306
-# dbUserRoot = 'root'
-# dbRootPassword = 'toor'
-# dbName = 'moneybaggyooodb'
-# clientTable = 'clients'
-# productTable = 'products'
-# productColumnNames = ['ProductID', 'ProductName', 'ProductPPU', 'ProductSellPrice']
-# productColumnTypes = ['int', 'varchar(255)', 'float', 'float']
-# clientColumnNames = ['ClientID', 'ClientFirst', 'ClientBalance']
-# clientColumnTypes = ['int', 'varchar(255)', 'float']
-# clientColumnNameTypeString = 'ClientID int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, ClientFirst varchar(255), ClientLast varchar(255), ClientBalance float'
-# productColumnNameTypeString = 'ProductID int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, ProductName varchar(255), ProductCostPerUnit float, ProductPricePerUnit float, ProductTotalAmount float'
-# tables = [clientTable, productTable]
-
 #Connect to DB if Exists
 def connection():
     dbHost = 'localhost'
@@ -28,10 +12,6 @@ def connection():
     clientTable = 'clients'
     productTable = 'products'
     assetTable = 'assets'
-    # productColumnNames = ['ProductID', 'ProductName', 'ProductPPU', 'ProductSellPrice']
-    # productColumnTypes = ['int', 'varchar(255)', 'float', 'float']
-    # clientColumnNames = ['ClientID', 'ClientFirst', 'ClientBalance']
-    # clientColumnTypes = ['int', 'varchar(255)', 'float']
     assetColumnNameTypeString = 'AssetID int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, AssetType varchar(255), AssetName varchar(255), AssetValue int'
     clientColumnNameTypeString = 'ClientID int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, ClientFirst varchar(255), ClientLast varchar(255), ClientBalance int'
     productColumnNameTypeString = 'ProductID int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, ProductName varchar(255), ProductCostPerUnit int, ProductPricePerUnit int, ProductTotalAmount int'
@@ -94,30 +74,3 @@ def connection():
 assetTupleList = [('Liquid', 'Cash', '200')]
 clientTupleList = [('John', 'Paz', '100'), ('Chico', 'Marino', '50'), ('Mike', 'Hummer', '200'), ('Walt', 'MindyMan', '50'), ('Stone', 'Lingo', '70'), ('Striker', 'VonFireworkin', '60'), ('Lee', 'Redard', '40'), ('Alexa', 'TashiFriend', '20')]
 productTupleList = [('Go Green Certificate', '4.28', '10.00', '28'), ('Software License', '32.14', '100.00', '20'), ('Software Hardening License', '32.14', '120.00', '0'), ('Modified Software License', '21.53', '80.00', '0')]
-
-# for i in clientTupleList:
-#     dbf.addFront(connection, cursor, i)
-
-# for i in productTupleList:
-#     dbf.addProducts(connection, cursor, i)
-
-# dbf.reduceProductTotal(connection, cursor, 'ProductTotalAmount', '80', 'Software License')
-
-# dbf.changeProductCost(connection, cursor, 'Go Green Certificate', '2.00')
-# dbf.changeProductPrice(connection, cursor, 'Go Green Certificate', '130.00')
-
-# command = f'SELECT ClientID FROM {clientTable} WHERE ClientFirst=\'Chico\''
-# cursor.execute(command)
-# cid = cursor.fetchone()
-# cid = cid[0]
-# dbf.addToBalance(connection, cursor, clientTable, cid, 20)
-#Create 'clients' column dictionary and the string of both names and types and DB table
-# clientColumnDict = dbf.createColumnDict(clientColumnNames, clientColumnTypes)
-# clientColumnNameTypeString = dbf.columnNameTypeString(clientColumnDict)
-
-# #Create 'products' column dictionary and the string of both names and types and DB table
-# productColumnDict = dbf.createColumnDict(productColumnNames, productColumnTypes)
-# productColumnNameTypeString = dbf.columnNameTypeString(productColumnDict)
-
-#Close DB cursor
-# cursor.close()
